67e37b585b73b500b2b1f582ad80d34dddb25eef
Sudhan <sudhandahake12@gmail.com>
2024-01-27 21:05:19 -0500

    Complete Login Form in WPF

---
+++ WPF_Front-End/View/UserControls/ClearableTextBox.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Swapify.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ClearableTextBox.xaml
+    /// </summary>
+    public partial class ClearableTextBox : UserControl
+    {
+        public ClearableTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private string placeholder;
+
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set
+            {
+                placeholder = value;
+
+                tbPlaceholder.Text = placeholder;
+            }
+        }
+
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInput.Text))
+            {
+                tbPlaceholder.Visibility = Visibility.Visible;
+            }
+
+            else
+            {
+                tbPlaceholder.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtInput.Clear();
+            txtInput.Focus();
+        }
+    }
+}
