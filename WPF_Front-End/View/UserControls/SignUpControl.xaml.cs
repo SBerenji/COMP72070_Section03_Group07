7012fd0afffe80e8fccf1256891734404ae4b9d7
Saba <sberenji4567@conestogac.on.ca>
2024-02-10 16:14:53 -0500

    Made the sign up form and integrated it with sudhan's log in form

--- WPF_Front-End/View/UserControls/SignUpControl.xaml.cs
+++ WPF_Front-End/View/UserControls/SignUpControl.xaml.cs
@@ -16,27 +16,22 @@ using System.Windows.Shapes;
 namespace WPF_Front_End.View.UserControls
 {
     /// <summary>
-    /// Interaction logic for Controls.xaml
+    /// Interaction logic for SignUpControl.xaml
     /// </summary>
-    public partial class Controls : UserControl
+    public partial class SignUpControl : UserControl
     {
-        public Controls()
+        public SignUpControl()
         {
             InitializeComponent();
         }
 
-        private void login_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Hello World");
-        }
-
-        private void createAccount_Click_1(object sender, RoutedEventArgs e)
+        private void LogIn_Click(object sender, RoutedEventArgs e)
         {
             Window parentWindow = Window.GetWindow(this);
 
-            SignUpForm signup = new SignUpForm();
+            MainWindow mainWindow = new MainWindow();
 
-            signup.Show();
+            mainWindow.Show();
 
             parentWindow.Close();
         }
