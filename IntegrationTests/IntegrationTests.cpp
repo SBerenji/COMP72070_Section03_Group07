ee834758946107856114102a7b450e6e828a18f6
Saba <sberenji4567@conestogac.on.ca>
2024-03-18 03:48:08 -0400

    Added some integration tests

--- IntegrationTests/IntegrationTests.cpp
+++ IntegrationTests/IntegrationTests.cpp
@@ -2,7 +2,7 @@
 #include "CppUnitTest.h"
 #include "../TCP_Client/Packet.h"
 #include "../TCP_Client/PacketWrapper.h"
-
+#include "string.h"
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 extern "C" int CloseSocket(SOCKET ClientSocket, closesocketFunc closeFunc = closesocket, WSACleanupFunc wsacleanupfunc = WSACleanup);
@@ -32,7 +32,7 @@ namespace IntegrationTests
 			Packet* packet = CreatePacket();
 			packet->GetBody()->Data = new char[10]; // Assuming data length is 10
 			packet->GetBody()->Data = "Testing";
-			packet->GetHead()->Length = 10; // Set the length
+			packet->GetHead()->Length = 10; // Set the length of the head
 
 			int totalSize = 0;  
 			char* serializedData = SerializeData(packet, totalSize); // Serialize the packet to get the serialized data
@@ -54,7 +54,162 @@ namespace IntegrationTests
 
 
 
+		///// <summary>
+		///// This test ensures that the sendData function can send a packet without any body to the server after estalishing a connection with the server
+		////  A true positive test
+		///// </summary>
+		TEST_METHOD(SendDataSuccessful_noPacketBody)
+		{
+			// Arrange
+			SOCKET clientSocket = setupConnection2();
+			std::string serverAddress = "127.0.0.1"; 
+
+			
+			Packet* packet = CreatePacket();
+			memcpy(packet->GetHead()->Source, serverAddress.c_str(), serverAddress.length());
+
+			std::string clientAddress = "127.0.0.1";
+			memcpy(packet->GetHead()->Destination, clientAddress.c_str(), clientAddress.length());
+
+			std::string route = "LOGIN";
+			memcpy(packet->GetHead()->Route, route.c_str(), route.length());
+
+			packet->GetHead()->Authorization = true;
+
+			packet->GetHead()->Length = 0;
+
+			int totalsize = sizeof(*(packet->GetHead()));
+
+			char* TxBuffer = new char[totalsize];
+
+			memset(TxBuffer, 0, totalsize);
+
+			memcpy(TxBuffer, packet->GetHead(), totalsize);
+
+
+
+			// Act
+			int sendDataResult = sendData(clientSocket, TxBuffer, totalsize);
+
+			FreeBuffer(TxBuffer);
+
+			DestroyPacket(packet);
+
+			// Assert
+			Assert::AreEqual(1, sendDataResult);
+
+		}
+
+		
+
 		// test the acutal funcitonality of the sendData function
+		//TEST_METHOD(SendDataSuccessful_PackeetWithBody)
+		//{
+		//	// Arrange
+		//	SOCKET clientSocket = setupConnection2();
+		//	std::string serverAddress = "127.0.0.1";
+
+
+		//	Packet* packet = CreatePacket();
+		//	memcpy(packet->GetHead()->Source, serverAddress.c_str(), serverAddress.length());
+
+		//	std::string clientAddress = "127.0.0.1";
+		//	memcpy(packet->GetHead()->Destination, clientAddress.c_str(), clientAddress.length());
+
+		//	std::string route = "LOGIN";
+		//	memcpy(packet->GetHead()->Route, route.c_str(), route.length());
+
+		//	packet->GetHead()->Authorization = true;
+
+		//	packet->GetHead()->Length = 0;
+
+
+
+
+
+
+		//	int totalsize = sizeof(*(packet->GetHead()));
+
+
+
+
+
+
+
+		//	char* TxBuffer = new char[totalsize];
+
+
+
+
+
+		//	// Act
+		//	int sendDataResult = sendData(clientSocket, TxBuffer, totalsize);
+
+		//	FreeBuffer(TxBuffer);
+
+		//	DestroyPacket(packet);
+
+		//	// Assert
+		//	Assert::AreEqual(1, sendDataResult);
+
+		//}
+
+
+
+		
+		///// <summary>
+		///// This test ensures that the setupConnection2 returns a clientsocket upon successfully establishing a connection with the server
+		//// This test should not pass whithout running the server
+		///// </summary>
+
+
+		/// <summary>
+		///  ****************************** needs fixing, currently a false positive test
+		/// </summary>
+		TEST_METHOD(SetUpConnection2_Successful)
+		{
+			// Arrange
+			SOCKET clientSocket;
+
+			// Act
+
+			clientSocket = setupConnection2();
+
+			// Assert 
+			Assert::AreNotEqual(INVALID_SOCKET, clientSocket);
+		}
+
+
+
+		///// <summary>
+		///// This test ensures that the setupConnection2 returns 0 if unsuccesful in establishing a connection to the server 
+		///// Server will not run while executing this test in order to show the functionality of the function
+		///// </summary>
+		TEST_METHOD(SetUpConnection2_Unsuccessful)
+		{
+			// Arrange
+			SOCKET clientSocket;
+
+			// Act
+
+			clientSocket = setupConnection2();
+
+			// Assert 
+			Assert::AreEqual((SOCKET)0, clientSocket);
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
 		// test the acutal funcitonality of the closeSocket function
 		
 
