37dc97123da77f3b3f5534958fd9483e94e1c853
Saba <sberenji4567@conestogac.on.ca>
2024-03-24 12:39:40 -0400

    Fixed the problems with the client funcitons for unit testing (compatibility issue with c#)

--- TCP_Client/Client.cpp
+++ TCP_Client/Client.cpp
@@ -9,50 +9,66 @@
 #include "Packet.h"
 #include "PacketWrapper.h"
 
-extern "C" __declspec(dllexport) int setupConnection();
+//extern "C" __declspec(dllexport) int setupConnection();
+extern "C" __declspec(dllexport) SOCKET setupConnection(WSAStartupFunc wsaStartup = WSAStartup, socketFunc socketfunc = socket, connectFunc connectfunc = connect, WSACleanupFunc wsacleanupfunc = WSACleanup);
 extern "C" __declspec(dllexport) SOCKET setupConnection2();
-extern "C" __declspec(dllexport) int sendData(SOCKET ClientSocket, char* TxBuffer, int totalSize);
+
+extern "C" __declspec(dllexport) int sendData(SOCKET ClientSocket, const char* TxBuffer, int totalSize);
+
+extern "C" __declspec(dllexport) int sendDataFunc(SOCKET ClientSocket, const char* TxBuffer, int totalSize, SendFunction sendFunc = send);
+
 extern "C" __declspec(dllexport) int recvData(SOCKET ClientSocket, char* RxBuffer, int RxBufferSize);
 extern "C" __declspec(dllexport) int CloseSocket(SOCKET ClientSocket);
+extern "C" __declspec(dllexport) int CloseSocketFunc(SOCKET ClientSocket, closesocketFunc closeFunc = closesocket, WSACleanupFunc wsacleanupfunc = WSACleanup);
 
-//extern "C" {
-//
-//    __declspec(dllexport) Packet* CreatePacket();
-//}
 
-// extern "C" __declspec(dllexport) int sendData(SOCKET ClientSocket);
-//extern "C" __declspec(dllexport) int main(int argc, char* argv[]);
 
-//void Print() {  
-//    std::cout << "Hello World!!" << std::endl;
-//}
-//
-//void Print2() {
-//    std::cout << "Hello World!!" << std::endl;
-//}
-//
 int CloseSocket(SOCKET ClientSocket) {
-    closesocket(ClientSocket);
+    return CloseSocketFunc(ClientSocket, closesocket, WSACleanup);
+}
+
+
+///// CHANGED THE CLOSESOCKET FUNCTION FOR TESTING PURPOSES
+int CloseSocketFunc(SOCKET ClientSocket, closesocketFunc closeFunc, WSACleanupFunc wsacleanupfunc)
+{
+
     // Cleaning up the socket
+    if (closeFunc(ClientSocket) == SOCKET_ERROR)
+    {
+        std::cerr << "Error closing the socket" << std::endl;
+        return 0;
+    }
 
     std::cout << "Client Socket Closed" << std::endl;
 
-    WSACleanup();
     // Cleaning up the winsock library
+    if (wsacleanupfunc() == SOCKET_ERROR)
+    {
+        std::cerr << "Error cleaning WSACleanup" << std::endl;
+        return 2;
+    }
 
     std::cout << "WSA Closed" << std::endl;
 
     return 1;
 }
 
-int sendData(SOCKET ClientSocket, char* TxBuffer, int totalSize) {
-    // The order matters, therefore the send is before receive on Client side.
-    // Send
 
-    //char sendBuffer[20] = "Hello World!";
-    // buffer to store address of the string
 
-    int sendSize = send(ClientSocket, TxBuffer, totalSize, 0);
+
+
+
+
+
+int sendData(SOCKET ClientSocket, const char* TxBuffer, int totalSize)
+{
+    return sendDataFunc(ClientSocket, TxBuffer, totalSize, send);
+}
+
+int sendDataFunc(SOCKET ClientSocket, const char* TxBuffer, int totalSize, SendFunction sendFunc)
+{
+
+    int sendSize = sendFunc(ClientSocket, TxBuffer, totalSize, 0);
     // here we are passing the clientSocket, the address of the string and the size of the string that we want the server to receive
     // +1 becuase of NULL terminator
 
@@ -60,9 +76,7 @@ int sendData(SOCKET ClientSocket, char* TxBuffer, int totalSize) {
     {
         std::cout << "Sending Failed" << std::endl;
 
-        closesocket(ClientSocket);
-
-        WSACleanup();
+        CloseSocket(ClientSocket);
 
         return 0;
     }
@@ -109,82 +123,17 @@ int recvData(SOCKET ClientSocket, char* RxBuffer, int RxBufferSize) {
     // returning 1 upon successful completion.
 }
 
-int setupConnection() {
-    // starting up and configuring the Winsock dynamically linked library
 
-    WSADATA wsaData;
 
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-        // providing the address of the object (&wsaData) into the library's startup function
-    {
-        std::cout << "ERROR: Failed to start WSA" << std::endl;
-
-        return 0;
-    }
-
-    else {
-        std::cout << "WSA Started" << std::endl;
-    }
-
-    // Create a Socket
-    // This is to make the call to the server
-
-    SOCKET ClientSocket;
-    ClientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    // here we are using TCP protocol
-
-    if (ClientSocket == INVALID_SOCKET)
-    {
-        WSACleanup();
-
-        std::cout << "ERROR: Failed to create ServerSocket" << std::endl;
-
-        return 0;
-    }
-
-    else {
-        std::cout << "Client Socket Created" << std::endl;
-    }
-
-    // Connect
-    // starting up the three way handshake process
-    // The server should have binded and configured a socket for Listening by now.
-
-    sockaddr_in SvrAddr;
-    // This object contains necessary information needed by the library to setup the TCP/IP headers correctly.
-
-    SvrAddr.sin_family = AF_INET;
-    // The family which specifies the protocol that will be used.
-
-    SvrAddr.sin_port = htons(27000);
-    // The client is using the port 27500.
-
-    SvrAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    // The IPV4 address the server is located on. Since different network we are gonna send the request to the NAT using localhost.
-
-    if ((connect(ClientSocket, (struct sockaddr*)&SvrAddr, sizeof(SvrAddr))) == SOCKET_ERROR)
-    {
-        closesocket(ClientSocket);
-        WSACleanup();
-
-        std::cout << "ERROR: Connection attempted Failed" << std::endl;
-
-        return 0;
-    }
-
-    else {
-        std::cout << "Connection Established" << std::endl;
-    }
-
-    return 1;
+// this is for compatibility with c# since c# does not support default parameters
+SOCKET setupConnection2()
+{
+    return setupConnection(WSAStartup, socket, connect, WSACleanup);
 }
 
 
 
-
-
-
-SOCKET setupConnection2() {
+SOCKET setupConnection(WSAStartupFunc wsaStartup, socketFunc socketfunc, connectFunc connectfunc, WSACleanupFunc wsacleanupfunc) {
     // starting up and configuring the Winsock dynamically linked library
 
     WSADATA wsaData;
@@ -297,4 +246,97 @@ SOCKET setupConnection2() {
 //
 //    //return 1;
 //    // returning 1 upon successful completion.
+//}
+
+
+
+
+
+//extern "C" {
+//
+//    __declspec(dllexport) Packet* CreatePacket();
+//}
+
+// extern "C" __declspec(dllexport) int sendData(SOCKET ClientSocket);
+//extern "C" __declspec(dllexport) int main(int argc, char* argv[]);
+
+//void Print() {  
+//    std::cout << "Hello World!!" << std::endl;
+//}
+//
+//void Print2() {
+//    std::cout << "Hello World!!" << std::endl;
+//}
+//
+
+
+//
+//int setupConnection() {
+//    // starting up and configuring the Winsock dynamically linked library
+//
+//    WSADATA wsaData;
+//
+//    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+//        // providing the address of the object (&wsaData) into the library's startup function
+//    {
+//        std::cout << "ERROR: Failed to start WSA" << std::endl;
+//
+//        return 0;
+//    }
+//
+//    else {
+//        std::cout << "WSA Started" << std::endl;
+//    }
+//
+//    // Create a Socket
+//    // This is to make the call to the server
+//
+//    SOCKET ClientSocket;
+//    ClientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+//    // here we are using TCP protocol
+//
+//    if (ClientSocket == INVALID_SOCKET)
+//    {
+//        WSACleanup();
+//
+//        std::cout << "ERROR: Failed to create ServerSocket" << std::endl;
+//
+//        return 0;
+//    }
+//
+//    else {
+//        std::cout << "Client Socket Created" << std::endl;
+//    }
+//
+//    // Connect
+//    // starting up the three way handshake process
+//    // The server should have binded and configured a socket for Listening by now.
+//
+//    sockaddr_in SvrAddr;
+//    // This object contains necessary information needed by the library to setup the TCP/IP headers correctly.
+//
+//    SvrAddr.sin_family = AF_INET;
+//    // The family which specifies the protocol that will be used.
+//
+//    SvrAddr.sin_port = htons(27000);
+//    // The client is using the port 27500.
+//
+//    SvrAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+//    // The IPV4 address the server is located on. Since different network we are gonna send the request to the NAT using localhost.
+//
+//    if ((connect(ClientSocket, (struct sockaddr*)&SvrAddr, sizeof(SvrAddr))) == SOCKET_ERROR)
+//    {
+//        closesocket(ClientSocket);
+//        WSACleanup();
+//
+//        std::cout << "ERROR: Connection attempted Failed" << std::endl;
+//
+//        return 0;
+//    }
+//
+//    else {
+//        std::cout << "Connection Established" << std::endl;
+//    }
+//
+//    return 1;
 //}
