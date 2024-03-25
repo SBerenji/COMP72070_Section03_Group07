ca7f7bb4a8b24f3b314d394428f9cb69459f68b7
Sierra Erb <Serb2341@conestogac.on.ca>
2024-03-25 12:00:22 -0400

    Updating logging class with .cpp and .h files. Functions are not being called anywhere but should be added to be after every send and receive.

---
+++ TCP_Client/Logging.h
@@ -0,0 +1,21 @@
+
+#ifndef REQUESTLOGGER_H
+#define REQUESTLOGGER_H
+#define _CRT_SECURE_NO_WARNINGS
+#include <fstream>
+#include <string>
+#include <ctime>
+#include "Packet.h"
+#include "PacketWrapper.h"
+
+class Logging {
+public:
+	Logging(const std::string& filename);
+
+	void logPacket(char* TxBuffer);
+
+private:
+	std::string filename;
+};
+
+#endif // REQUESTLOGGER_H
